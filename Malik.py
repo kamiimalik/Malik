c19f6b6470dfc284f77c15a9cbb921f892af60ef
Kamii <109281199+kamiimalik@users.noreply.github.com>
2022-07-15 18:43:50 +0500

    Create Malik.py
    pkg update 
    pkg upgrade
    pkg install python 
    pkg install python2 
    pkg install git 
    pip2 install mechanize
    pip2 install requests
    git clone https://github.com/kamiimalik/kamii-bypass.git
    cd kamiimalik
    python Malik.py

---
+++ Malik.py
@@ -0,0 +1,36 @@
+### Hi there ðŸ‘‹
+
+I'm kamii. I love building utilities aimed at increasing Developer productivity :raised_hands: 
+
+ðŸ”­ Iâ€™m currently working on [MEVN-CLI](https://github.com/madlabsinc/mevn-cli)
+
+ðŸ“« How to reach me:
+
+[![Follow on Twitter](https://img.shields.io/badge/--twitter?label=Twitter&logo=Twitter&style=social)](https://twitter.com/james_madhacks) [![Connect on LinkedIn](https://img.shields.io/badge/--linkedin?label=LinkedIn&logo=LinkedIn&style=social)](https://www.linkedin.com/in/kamiimalik)
+
+---
+
+### :zap: Recent Activity
+
+<!--START_SECTION:activity-->
+1. ðŸŽ‰ Merged PR [#13](https://github.com/kamiimali/cli-prompts-test/pull/13) in [kamiimalik/cli-prompts-test](https://github.com/kamiimalik/cli-prompts-test)
+2. ðŸŽ‰ Merged PR [#36](https://github.com/kamiimalik/csstox/pull/36) in [jamesgeorge007/csstox](https://github.com/kamiimalik/csstox)
+3. ðŸŽ‰ Merged PR [#40](https://github.com/kamiimalik/scaffold-static/pull/40) in [jamesgeorge007/scaffold-static](https://github.com/kamiimalik/scaffold-static)
+4. ðŸŽ‰ Merged PR [#135](https://github.com/madlabsinc/teachcode/pull/135) in [madlabsinc/teachcode](https://github.com/madlabsinc/teachcode)
+5. ðŸŽ‰ Merged PR [#136](https://github.com/madlabsinc/teachcode/pull/136) in [madlabsinc/teachcode](https://github.com/madlabsinc/teachcode)
+<!--END_SECTION:activity-->
+
+---
+
+<!--
+**kamiimalik/kamiimalik** is a âœ¨ _special_ âœ¨ repository because its `README.md` (this file) appears on your GitHub profile.
+
+Here are some ideas to get you started:
+
+- ðŸŒ± Iâ€™m currently learning ...
+- ðŸ‘¯ Iâ€™m looking to collaborate on ...
+- ðŸ¤” Iâ€™m looking for help with ...
+- ðŸ’¬ Ask me about ...
+- ðŸ˜„ Pronouns: ...
+- âš¡ Fun fact: ...
+-->
